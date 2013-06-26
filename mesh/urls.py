046554962a808f7e6f44f54a49c84352fca34ff4
Kris <kris@mammothlabs.com.au>
2013-06-26 18:16:55 +1000

    Server stuff

--- mesh/urls.py
+++ mesh/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.decorators import login_required
@@ -38,3 +39,8 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.contrib.staticfiles.views' , {'document_root': settings.STATIC_ROOT}),
+    )
