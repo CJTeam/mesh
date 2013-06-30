8c11f6d3ef3e44d7f519b82e1a2f808aebf21529
baton@gallop <ops@2-9-1.com>
2013-06-30 13:43:14 +1000

    building vault

--- vault/main.py
+++ vault/main.py
@@ -18,24 +18,32 @@ store.local_create("g20_summit")
 value.node_create("g20_summit", {"Label": "Obama"})
 
 # Create an edge 
-value.edge_create("g20_summit", {"Source": "Obama", "Target": "Obama", "Type": "Undirected", "Label": "self", "Weight": "0"})
+value.edge_create("g20_summit", {"Source": "Obama", "Target": "Obama", "Type": "Undirected", "Label": "US-US", "Weight": "0"})
 
-# Add 'Country' attribute to the Node table.
+# Add 'Country' attribute to the node table
 attr.node_create("g20_summit", "Country")
 
-# Add 'Relationship' attribute to the Edge table.
+# Add 'Relationship' attribute to the edge table
 attr.edge_create("g20_summit", "Relationship")
 
-# Build Nodes.
+# Build nodes
 value.node_create("g20_summit", {"Label": "Gillard" , "Country": "AU"})
 value.node_create("g20_summit", {"Label": "Merkel"  , "Country": "DE"})
 value.node_create("g20_summit", {"Label": "Ayrault" , "Country": "FR"})
 value.node_create("g20_summit", {"Label": "Cameron" , "Country": "UK"})
 value.node_create("g20_summit", {"Label": "Abdullah", "Country": "SA"})
 
+# Build edges
+value.edge_create("g20_summit", {"Source": "Gillard", "Target": "Gillard", "Type": "Undirected", "Label": "AU-AU", "Weight": "0", "Relationship": "Self"})
+value.edge_create("g20_summit", {"Source": "Merkel", "Target": "Merkel", "Type": "Undirected", "Label": "DE-DE", "Weight": "0", "Relationship": "Self"})
+value.edge_create("g20_summit", {"Source": "Ayrault", "Target": "Ayrault", "Type": "Undirected", "Label": "FR-FR", "Weight": "0", "Relationship": "Self"})
+value.edge_create("g20_summit", {"Source": "Cameron", "Target": "Cameron", "Type": "Undirected", "Label": "UK-UK", "Weight": "0", "Relationship": "Self"})
+value.edge_create("g20_summit", {"Source": "Abdullah", "Target": "Abdullah", "Type": "Undirected", "Label": "SA-SA", "Weight": "0", "Relationship": "Self"})
+
 raise Exception("NOT IMPLEMENTED main")
 
 value.node_update("g20_summit", {"Label": "Obama", "Country": "US"})
+value.edge_update("g20_summit", {"Source": "Obama", "Target": "Obama", "Type": "Undirected", "Label": "US-US", "Weight": "0", "Relationship": "Self"})
 
 # Build Edges
 value.edge_create((("Source","Obama"),("Target","Gillard"),("Type","Directed"),("Label","US-AU"),("Weight",10),("Relationship","Cordial")))
